010636862c3a83597d01a2ded207f39b4f0c8794
Quentin Lambotte <quentin.lambotte@hotmail.com>
2022-05-31 11:14:34 +0200

    Add request version

--- dmy_all_paper/webcam/main.py
+++ dmy_all_paper/webcam/main.py
@@ -10,22 +10,33 @@ from FCOS.fcos_core.predictor import COCODemo
 from DMY.dt_and_rec import DetectRecognizeDmy
 from DMY.dmy_utils import load_networks
 
-def main(path_to_picture):
-    if getattr(sys, 'frozen', False):
-        path = Path(sys._MEIPASS)
-        print(path)
-    else:
-        path = Path(__file__).parent
-        print(path)
-    num_gpus = torch.cuda.device_count()
-    print('# of GPUs: {}'.format(num_gpus))
-    args, cfg_fcos = fcos_cfgs(path)
-    models = load_networks(path)
-    detect_rec_dmy = DetectRecognizeDmy(models)
-    coco_demo = COCODemo(cfg_fcos,
-      confidence_thresholds_for_classes=(args.thresholds_for_classes),
-      min_image_size=(args.min_image_size))
-    image = cv2.imread(path_to_picture)
+if getattr(sys, 'frozen', False):
+    path = Path(sys._MEIPASS)
+    print(path)
+else:
+    path = Path(__file__).parent
+    print(path)
+num_gpus = torch.cuda.device_count()
+print('# of GPUs: {}'.format(num_gpus))
+args, cfg_fcos = fcos_cfgs(path)
+models = load_networks(path)
+detect_rec_dmy = DetectRecognizeDmy(models)
+coco_demo = COCODemo(cfg_fcos,
+                     confidence_thresholds_for_classes=(args.thresholds_for_classes),
+                     min_image_size=(args.min_image_size))
+
+def img_from_request(request_data):
+    return cv2.imdecode(
+        np.frombuffer(request_data.read(), np.uint8),
+        cv2.IMREAD_COLOR
+    )
+
+def img_from_path(path):
+    return cv2.imread(path)
+
+
+def main(image):
+    "Image must be of the same type as of the return type of cv2.imread"
     start_time=time.time()
     image, date_images, tl_info = coco_demo.run_on_opencv_image(image)
     if date_images is not None:
@@ -38,7 +49,7 @@ def main(path_to_picture):
 
 if __name__ == '__main__':
     path_pic = sys.argv[1]
-    result, t = main(path_pic)
+    result, t = main(img_from_path(path_pic))
     print(result)
     print(f"In {t} s")
 # okay decompiling webcam.pyc
